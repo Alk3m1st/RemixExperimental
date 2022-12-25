7d262066ba1c805b44b22f32983aee16f55dd0ec
Allan Oliveira <r9oliveira@gmail.com>
2022-12-25 22:43:23 +0800

    Add Nav and allow client side ENV vars on window

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,8 +7,9 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-
+import { Nav } from "./components/navigation/Nav";
 import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
@@ -18,7 +20,17 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export async function loader() {
+  return json({
+    ENV: {
+      AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
+      AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
+    },
+  });
+}
+
 export default function App() {
+  const data = useLoaderData<typeof loader>();
   return (
     <html lang="en">
       <head>
@@ -26,8 +38,16 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <Nav />
+        <main className="container mx-auto p-4">
+          <Outlet />
+        </main>
         <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+          }}
+        />
         <Scripts />
         <LiveReload />
       </body>
---
+++ typings.d.ts
@@ -0,0 +1,3 @@
+declare interface Window {
+  ENV: { AIRTABLE_API_KEY: string; AIRTABLE_BASE_ID: string };
+}
